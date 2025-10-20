c2da26eb094e432e541a504dc3fd76adbd02dafb
Miruthul kumar M <127958188+Miruthulkumar@users.noreply.github.com>
2025-10-20 22:54:06 +0530

    feat: implement purchase confirmation and discount logging in Courses component

--- learning-react/src/components/Courses.jsx
+++ learning-react/src/components/Courses.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 const Courses = (props) => {
+  const [purchased, setPurchased] = useState(false);
+
   function buyNow(discount) {
     console.log(props.name + ` Course Bought! with ${discount}% Discount! `);
   }
@@ -17,9 +19,18 @@ const Courses = (props) => {
           <p className="text-gray-600 mb-2">Duration: {props.duration}</p>
           <p className="mb-2">{props.rating}</p>
           <p className="text-green-600 font-bold">{props.price}</p>
-          <button onClick={()=>buyNow(10)} className="bg-gray-500 rounded-2xl hover:bg-gray-600 transition-colors duration-100 mt-2.5 mr-40">
+          <button
+            onClick={() => {
+              buyNow(10);
+              setPurchased(true);
+            }}
+            className="bg-gray-500 rounded-2xl hover:bg-gray-600 transition-colors duration-100 mt-2.5 mr-40"
+          >
             Buy Now
           </button>
+          <p className="text-gray-400 ml-2 p-1.5">
+            {purchased ? "Thanks For Buying!" : "Few Seats left!!!"}
+          </p>
         </div>
       </div>
     )
